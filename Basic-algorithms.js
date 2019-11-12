76752f4821a2d0bc375e16624d60149c9af60a89
biancacraciun <craciunbiancas@yahoo.com>
2019-11-12 19:44:17 +0200

    Basic alg update

--- Basic-algorithms.js
+++ Basic-algorithms.js
@@ -373,7 +373,7 @@ function test(n) {
 };
 
 function select_nr (arr, fn) {
-  for (var i = 0; i <= arr.length; ) {
+  for (var i = 0; i <= arr.length; i++) {
     if (fn(arr[i])) {
       return arr[i];
     } else {
